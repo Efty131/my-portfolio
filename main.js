7f1673886efc17b8512be43dffc2a4ed5be3d0cb
Efty <efty131331@gmail,com>
2021-08-14 11:40:43 +0600

    everything almost done

--- main.js
+++
@@ -1,8 +0,0 @@
-const burger = document.getElementById("burger");
-const list = document.getElementById("list");
-
-burger.addEventListener("click", () => {
-  list.style.display !== "none"
-    ? (list.style.display = "none")
-    : (list.style.display = "flex");
-});
